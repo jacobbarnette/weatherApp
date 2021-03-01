2563c46d26b5a7691e438916c91d99e0329f8c68
Jacob Barnette <jacobcbarnette@gmail.com>
2021-03-01 15:54:56 -0500

    rough styling, added search bar

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const cityName = 'Los Angeles';
+let cityName = 'Los Angeles';
 const apiKey = 'a0e789560293d5ccee72e778407f9e19';
 let unit ='imperial'
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${unit}&appid=${apiKey}`;
@@ -7,11 +7,9 @@ async function getWeather(){
     const response = await fetch(url, {mode:'cors'});
     const data = await response.json();
    formatWeather(data);
-   console.log(data);
-}
+  
+}getWeather();
 
-
-getWeather();
 
 function formatWeather(data){
     let newWeather = {
@@ -21,43 +19,38 @@ function formatWeather(data){
         feelsLike: Math.round(data.main.feels_like),
         humidity: Math.round(data.main.humidity),
         windSpeed: Math.round(data.wind.speed)
-
     }
-    console
-    console.log(newWeather.name)
-    console.log(newWeather.weather);
-    console.log(newWeather.temp);
-    console.log(newWeather.feelsLike);
-    console.log(newWeather.humidity);
-    console.log(newWeather.windSpeed);
-   
-
     /*get wind degrees and convert to direction*/
-    
-    const directions = ['north', 'northeast', 'east', 'southeast', 'south', 'southwest','west', 'northwest'];
+    const directions = ['N', 'NE', 'E', 'SE', 'S', 'SW','W', 'NW'];
     let degrees = 10;
     degrees = degrees * 8 / 360;
     degrees = Math.round(degrees, newWeather.windSpeed);
     degrees = (degrees + 8) % 8;
     newWeather.windDirection = directions[degrees];
-   
-
     createWeatherCard(newWeather);
 }
 
 function createWeatherCard(newWeather){
     const cityWeather = document.getElementById('cityWeather');
-    cityWeather.textContent = newWeather.weather
+    cityWeather.textContent = `${newWeather.weather}`
     const cityHeader = document.getElementById('cityHeader');
     cityHeader.textContent = newWeather.name;
     const cityTemp = document.getElementById('cityTemp');
-    cityTemp.textContent = newWeather.temp;
+
+    cityTemp.textContent = `${newWeather.temp} ${'\xB0'} `;
     const feelsLike = document.getElementById('feelsLike');
-    feelsLike.textContent = newWeather.feelsLike;
+    feelsLike.textContent = `Feels Like: ${newWeather.feelsLike} ${'\xB0'}`;
     const humidty = document.getElementById('humidity');
-    humidty.textContent = newWeather.humidty;
+    humidty.textContent = `Humidity: ${newWeather.humidity} %`;
     const windSpeed = document.getElementById('windSpeed');
-    windSpeed.textContent = newWeather.windSpeed;
-    const windDirection = document.getElementById('windDirection');
-    windDirection.textContent = newWeather.windDirection;
+    windSpeed.textContent = `Wind Speed: ${newWeather.windSpeed} MPH ${newWeather.windDirection}`
 }
+
+const searchCity = document.getElementById('searchCity');
+searchCity.addEventListener('click', async function(e){
+    e.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchBar.value}&units=${unit}&appid=${apiKey}`;
+    const response = await fetch(url, {mode:'cors'});
+   const data = await response.json();
+  formatWeather(data);
+});
